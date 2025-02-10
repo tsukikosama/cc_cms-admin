2f22b56ce83ebeb8a1f54b26b2a5871da2415c9d
weilai <1083344129@qq.com>
2025-02-10 12:04:06 +0800

    [fix] 删除无用内容

---
+++ src/main/java/com/weilai/ccSpringboot/modules/controller/DriftBottleController.java
@@ -0,0 +1,11 @@
+package com.weilai.ccSpringboot.modules.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/drift")
+public class DriftBottleController {
+
+//    private final StringT
+}
